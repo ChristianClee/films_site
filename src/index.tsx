adb4f27ccdc25539a05c67ba02b0358458971287
ChristianClee <ilya.mishkov@gmail.com>
2023-11-06 15:49:38 +0300

    BrowserRouter changed to HashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
 import { store, persistor } from './redux/store/store'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react';
@@ -12,12 +12,12 @@ const root = ReactDOM.createRoot(
 );
 root.render(
 
-  <BrowserRouter>
+  <HashRouter>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <App />
       </PersistGate>
     </Provider>
-  </BrowserRouter>
+  </HashRouter>
 
 );
